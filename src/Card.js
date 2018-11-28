ed87f1c14fa76c401db7094ea6013f9afbe4cb67
Justin Torrance <justin.torrance91@gmail.com>
2018-11-28 15:52:28 -0700

    Render years and values to page

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const Card = ({location, stats}) => {
+  console.log(location)
+  console.log(stats)
+  const year = Object.keys(stats).map(year => (year))
+  const value = Object.values(stats).map(value => (value))
+  const yearlyValue = year.map((yearlyValue, i) => (<li>{year[i]}: {value[i]}</li>))
+
   return(
     <div>
-      <h1>{location}</h1>
+      <h3>{location}</h3>
+        <ul>
+          {yearlyValue}
+        </ul>
     </div>
   );
 }
